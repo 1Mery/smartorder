ff18592ff028047a3517e612ff9724fb4ede149f
1Mery <meryem.ozevren442001@gmail.com>
2025-12-19 23:12:52 +0300

    update order-service dev config and kafka publisher

--- order-service/src/main/java/com/turkcell/orderservice/infrastructure/kafka/KafkaOrderEventPublisher.java
+++ order-service/src/main/java/com/turkcell/orderservice/infrastructure/kafka/KafkaOrderEventPublisher.java
@@ -1,5 +1,9 @@
 package com.turkcell.orderservice.infrastructure.kafka;
 
+import com.turkcell.orderservice.application.event.OrderCancelledEvent;
+import com.turkcell.orderservice.application.event.OrderCreatedEvent;
+import com.turkcell.orderservice.application.event.OrderDeliveredEvent;
+import com.turkcell.orderservice.application.event.OrderPaidEvent;
 import com.turkcell.orderservice.application.ports.OrderEventPublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
@@ -16,9 +20,41 @@ public class KafkaOrderEventPublisher implements OrderEventPublisher {
     }
 
     @Override
-    public void publish(Object event) {
+    public void publishOrderCreated(OrderCreatedEvent event) {
+        boolean sent=streamBridge.send("orderCreateEvents-out-0",event);
+        if(!sent){
+            log.error("Order event could not sent to kafka");
+        }
+        else {
+            log.info("Order event sent to kafka"+event);
+        }
+    }
 
-        boolean sent=streamBridge.send("orderEvents-out-0",event);
+    @Override
+    public void publishOrderPaid(OrderPaidEvent event) {
+        boolean sent=streamBridge.send("orderPaidEvents-out-0",event);
+        if(!sent){
+            log.error("Order event could not sent to kafka");
+        }
+        else {
+            log.info("Order event sent to kafka"+event);
+        }
+    }
+
+    @Override
+    public void publishOrderDelivered(OrderDeliveredEvent event) {
+        boolean sent=streamBridge.send("orderDeliveredEvents-out-0",event);
+        if(!sent){
+            log.error("Order event could not sent to kafka");
+        }
+        else {
+            log.info("Order event sent to kafka"+event);
+        }
+    }
+
+    @Override
+    public void publishOrderCancelled(OrderCancelledEvent event) {
+        boolean sent=streamBridge.send("orderCancelEvents-out-0",event);
         if(!sent){
             log.error("Order event could not sent to kafka");
         }
